a3b2af502fc0657f7870c54993a022645ccd1f3e
Arika85 <arikasunkari@gmail.com>
2025-02-04 18:10:18 +0530

    data types

---
+++ src/com/assigments/Assignment1_DataTypes.java
@@ -0,0 +1,88 @@
+package com.assigments;
+
+public class Assignment1_DataTypes {
+
+	public static void main(String[] args) {
+		
+		
+		float temperature = 25.5f;
+		boolean orderPlaced = true;
+		boolean orderNotPlaced = false;
+		String phoneNumber = "123-456-7890";
+		float money = 1000.50f;
+		String emailAdd = "john.doe@example.com";
+		
+		float lattitude = 37.7749f;
+		float longitude = -122.4194f;
+		String latLong = "37.7749f, -122.4194f";
+		
+		
+		boolean married = true;
+		boolean unmarried = false;
+		
+		String occupation = "Software Engineer";
+		String color = "Blue";
+		int year = 2023;
+		String followers = "1,000,000";
+		int mediaFollowers = 1000000;
+		float rating = 7.5f;
+		char bloodType = 'A';
+		String title = "To Kill a Mockingbird";
+		int employees = 500;
+		String time = "2:30 PM";
+		String country = "United States";
+		String eyeColor = "Brown";
+		String birthPlace = "New York City";
+		float distance = 200.5f;
+		
+		
+		
+		
+		
+		System.out.println("Temperature of City in degrees Celsius : " + temperature);
+		
+		System.out.println("whether customer placed an order : " + orderPlaced + " or " + orderNotPlaced);
+		System.out.println("If customer placed an order : " + orderPlaced);
+		System.out.println("If customer not placed an order : " + orderNotPlaced);
+		
+		System.out.println("Peron's Phone number : " + phoneNumber);
+		System.out.println("Amount of money in a customer's bank account : " + money);
+		System.out.println("Person's email address: " + emailAdd);
+		
+		System.out.println("Coordinates of a location (latitude, longitude): " + lattitude + ", " + longitude);
+		System.out.println("Coordinates of a location (latitude, longitude): " + latLong);
+		
+		System.out.println("Person's marital status: " + married + " or " + unmarried);
+		System.out.println("Person's occupation: " + occupation);
+		System.out.println("Person's favourite colour: " + color);
+		System.out.println("Current year : " + year);
+		
+		System.out.println("Number of followers on a social media platform:" + followers);
+		System.out.println("Number of followers on a social media platform:" + mediaFollowers);
+		
+		System.out.println("Rating of a movie: " + rating);
+		System.out.println("Person's blood type: " + bloodType);
+		System.out.println("Title of a book: " + title);
+		System.out.println("Number of employees in a company: " + employees);
+		
+//		which datatype time, money separated with commas should be considered 
+		System.out.println("Time of an event: " + time);
+		System.out.println("Name of a country: " + country);
+		
+		System.out.println("Person's eye color: " + eyeColor);
+		System.out.println("Person's birthplace: " + birthPlace);
+		System.out.println("Distance between two cities : " + distance);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
